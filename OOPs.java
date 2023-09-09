506f81e5540f8a8a26b7312c7e29957a9eaadd46
Thewhite33 <rohitrc2005@gmail.com>
2023-09-09 12:52:03 +0530

    bit complete

---
+++ OOPs.java
@@ -0,0 +1,15 @@
+public class OOPs {
+    public static void main
+}
+class Pen{
+    String color;
+    int tip;
+
+    void setColor(String newColor){
+        color = newColor;
+    }
+
+    void setTip(int newTip){
+        tip = newTip;
+    }
+}
--- bitman.java
+++ bitman.java
@@ -47,6 +47,30 @@ public class bitman{
         int bitMask = a|b;
         return n & bitMask;
     }
+    public static boolean isPowerOfTwo(int n){
+        return(n&(n-1))==0;
+    }
+    public static int countBit(int n){
+        int count = 0;
+        while(n>0){
+            if((n&1) != 0){
+                count++;
+            }
+            n = n>>1;
+        }
+        return count;
+    }
+    public static int fastExpo(int a,int n){
+        int ans = 1;
+        while(n>0){
+            if((n&1)!=0){
+                ans = ans*a;
+            }
+            a = a*a;
+            n = n>>1;
+        }
+        return ans;
+    }
     public static void main(String args[]){
         // System.out.println(5|6);
         // System.out.println(5&6); //Use single &
@@ -61,6 +85,11 @@ public class bitman{
         //System.out.println(clearIthBit(10, 1));
         //System.out.println(updateIthBit(10, 2, 1));
         //System.out.println(clearIBits(15, 2));
-        System.out.println(clearIbits(10, 2,4));
+        //System.out.println(clearIbits(10, 2,4));
+        //System.out.println(isPowerOfTwo(9));
+        //System.out.println(countBit(100));
+        System.out.println(fastExpo(2, 100));
+    
+    
     }
 }
