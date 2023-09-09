191c818c156396f7f63b3fa921e36081c3357c78
Thewhite33 <rohitrc2005@gmail.com>
2023-09-09 13:17:35 +0530

    oops doing

--- OOPs.java
+++ OOPs.java
@@ -1,15 +1,37 @@
 public class OOPs {
-    public static void main
+    public static void main(String args[]){
+        Pen p1 = new Pen();
+        p1.setColor("Blue");
+        System.out.println(p1.color);
+        p1.setTip(5);
+        System.out.println(p1.tip);
+
+        bankAcc myAcc = new bankAcc();
+        myAcc.username = "Rohit";
+        //myAcc.password = "adcfjdu";   //Password is not visible
+        myAcc.setPassword("hellowb");
+    }
 }
 class Pen{
     String color;
     int tip;
 
+    String getColor(){
+        return this.color;
+    }
     void setColor(String newColor){
         color = newColor;
     }
 
     void setTip(int newTip){
         tip = newTip;
+    }
+}
+
+class bankAcc{
+    public String username;
+    private String password;
+    public void setPassword(String pwd){
+        password = pwd;
     }
 }
