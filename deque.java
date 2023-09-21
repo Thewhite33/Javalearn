1e79ba0f6ac39907a9928b369764e9c1f8a39923
Thewhite33 <rohitrc2005@gmail.com>
2023-09-21 21:55:40 +0530

    nw

--- deque.java
+++ deque.java
@@ -1,6 +1,32 @@
 import java.util.*;
 public class deque {
+    static class Stack{
+        Deque<Integer> d = new LinkedList<>();
+        public void push(int data){
+            d.addLast(data);
+        }
+        public int pop(){
+            return d.removeLast();
+        }
+        public int peek(){
+            return d.getLast();
+        }
+    }
     public static void main (String args[]){
-        
+        // Deque<Integer> q = new LinkedList<>();
+        // q.addFirst(1);
+        // q.addFirst(2);
+        // System.out.println(q);
+        // q.removeFirst();
+        // System.out.println(q);
+
+        // //getFirst and getLast
+
+
+        Stack s = new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        System.out.println("peek = "+s.peek());
     }
 }
